a4a5e1ac534b3043f758f98c457f1981546a4df1
Okera Johnson <okerajohnson@gmail.com>
2024-09-17 10:43:48 -0400

    quote app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,6 +7,25 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+
+  const [Quote, setQuote] = useState('Loading');
+  const [Author, setAuthor] = useState('Loading');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const randomQuote = () => {
+    setIsLoading(true);
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+    console.log(result.content);
+    setQuote(result.content);
+    setAuthor(result.content);
+    setIsLoading(false);
+  })
+}
+
+  useEffect(() => {
+    randomQuote();
+  }, []);
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -15,37 +35,22 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+     <View style={styles.container}>
+      <Text style={{ fontSize: 60, marginBottom: -20}}> {Quote} </Text>
+      <Text style={{ fontSize: 60, marginBottom: -20}}> {Author} </Text>
+      <TouchableOpacity
+        onPress={randomQuote}
+          style={{
+            backgroundColor: '#5372F0',
+            padding: 20,
+            borderRadius: 30,
+            marginVertical: 20,
+          }}>
+            <Text style={{color: '#fff', fontSize: 18, textAlign: 'center'}}>
+              {isLoading ? "Loading..." : "New Quote"}
+            </Text>
+          </TouchableOpacity>
+     </View>
     </ParallaxScrollView>
   );
 }
@@ -67,4 +72,10 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
 });
